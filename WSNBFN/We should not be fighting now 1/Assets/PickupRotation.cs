1ebe9e1ad6b5a983137b2cf925e80938150cfb81
franavils <aviles110@hotmail.com>
2016-06-05 15:44:22 +0200

    New Colours and PickUp

---
+++ WSNBFN/We should not be fighting now 1/Assets/PickupRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupRotation : MonoBehaviour {
+
+    public float RotationSpeed;
+    float currentXRotation;
+    Transform t;
+
+	// Use this for initialization
+	void Start () {
+
+        t = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        currentXRotation = t.rotation.x;
+        t.Rotate(0, currentXRotation + RotationSpeed, 0);
+	}
+}
